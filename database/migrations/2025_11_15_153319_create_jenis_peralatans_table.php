05a00c53543f2187601621c8a896db69a4ffaf89
Abraham Miko <abrahammikopratama03@gmail.com>
2025-11-27 15:46:38 +0700

    pergantian migrasi

--- database/migrations/2025_11_15_153319_create_jenis_peralatans_table.php
+++ database/migrations/2025_11_15_153319_create_jenis_peralatans_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('mobil', function (Blueprint $table) {
-            $table->id();
-            $table->string('plat_nomor', 50)->unique();
+        Schema::create('jenis_peralatans', function (Blueprint $table) {
+            $table->id('id_jenis_peralatan');
+            $table->enum('kategori', ['console', 'vr', 'display']);
             $table->string('merek');
-            $table->year('tahun');
-            $table->integer('kapasitas');
-            $table->string('foto_mobil');
-            $table->enum('status_kendaraan', ['tersedia', 'dirental', 'perawatan', 'nonaktif'])->default('tersedia');
+            $table->decimal('harga_rental_per_hari', 10, 2);
+            $table->text('deskripsi');
+            $table->string('foto_peralatan');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('mobil');
+        Schema::dropIfExists('jenis_peralatans');
     }
 };
