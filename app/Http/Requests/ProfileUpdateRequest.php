0dbba971fe812ac402001a84e56263ce905bbfdc
FrlFrz <farel.purnomosasi20@gmail.com>
2025-12-05 11:42:02 +0700

    perbaikan halaman profile-edit

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,6 +17,8 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
+            'nama_depan' => ['required', 'string', 'max:255'],
+            'nama_belakang' => ['required', 'string', 'max:255'],
             'email' => [
                 'required',
                 'string',
